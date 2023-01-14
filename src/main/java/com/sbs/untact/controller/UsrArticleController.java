9055d91a1f0a684a403c7c588996f57352e23e74
taejong12 <dbxowhdsla12@naver.com>
2023-01-14 22:52:19 +0900

    게시물에 수정날짜 컬럼 추가

--- src/main/java/com/sbs/untact/controller/UsrArticleController.java
+++ src/main/java/com/sbs/untact/controller/UsrArticleController.java
@@ -27,8 +27,8 @@ public class UsrArticleController {
 		
 		// 게시물 2개 생성
 		articles= new ArrayList<>();
-		articles.add(new Article(++articlesLastId, "2020-12-12 12:12:12", "제목1", "내용1"));
-		articles.add(new Article(++articlesLastId, "2020-12-12 12:12:12", "제목2", "내용2"));
+		articles.add(new Article(++articlesLastId, "2020-12-12 12:12:12", "2020-12-12 12:12:12", "제목1", "내용1"));
+		articles.add(new Article(++articlesLastId, "2020-12-12 12:12:12", "2020-12-12 12:12:12", "제목2", "내용2"));
 	
 	}
 	
@@ -55,8 +55,9 @@ public class UsrArticleController {
 		// String regDate = format1.format(time);
 		
 		String regDate = Util.getNowDateStr();
+		String updateDate = regDate;
 		
-		articles.add(new Article(++articlesLastId, regDate, title, body));
+		articles.add(new Article(++articlesLastId, regDate, updateDate, title, body));
 		
 		Map<String, Object> rs = new HashMap<>();
 		rs.put("resultCode", "S-1");
@@ -118,6 +119,7 @@ public class UsrArticleController {
 			return rs;
 		} 
 		
+		selArticle.setUpdateDate(Util.getNowDateStr());
 		selArticle.setTitle(title);
 		selArticle.setBody(body);
 		
